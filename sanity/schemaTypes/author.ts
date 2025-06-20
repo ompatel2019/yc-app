11812612099ec0f1fadb441985ee5fbf7ee9a87a
Om Patel <ompatel2019@gmail.com>
2025-06-20 19:43:39 +1000

    added schemas to sanity sucessfully

--- sanity/schemaTypes/author.ts
+++ sanity/schemaTypes/author.ts
@@ -1,5 +1,5 @@
 import { defineField, defineType } from "sanity";
-import { UserIcon } from "lucide-react";
+import { UserIcon } from "@sanity/icons";
 
 export const author = defineType({
   name: "author",
@@ -7,34 +7,16 @@ export const author = defineType({
   type: "document",
   icon: UserIcon,
   fields: [
-    defineField({
-      name: "id",
-      type: "number",
-    }),
-    defineField({
-      name: "name",
-      type: "string",
-    }),
-    defineField({
-      name: "username",
-      type: "string",
-    }),
-    defineField({
-      name: "email",
-      type: "string",
-    }),
-    defineField({
-      name: "image",
-      type: "url",
-    }),
-    defineField({
-      name: "bio",
-      type: "text",
-    }),
+    defineField({ name: "name", type: "string" }),
+    defineField({ name: "username", type: "string" }),
+    defineField({ name: "email", type: "string" }),
+    defineField({ name: "image", type: "url" }),
+    defineField({ name: "bio", type: "text" }),
   ],
   preview: {
     select: {
       title: "name",
+      subtitle: "email",
     },
   },
 });
