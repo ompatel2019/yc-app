e7d3fd00527ea7e253bae786ddc43566f59e975b
Om Patel <ompatel2019@gmail.com>
2025-06-22 12:53:41 +1000

    sanity-user login functionality

--- auth.ts
+++ auth.ts
@@ -1,13 +1,60 @@
-import { NextAuthOptions, getServerSession } from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
+import { getServerSession } from "next-auth/next";
+import GitHub from "next-auth/providers/github";
+import { client } from "@/sanity/lib/client";
+import { writeClient } from "@/sanity/lib/write-client";
+import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries";
 
-export const authOptions: NextAuthOptions = {
+export const authOptions = {
   providers: [
-    GitHubProvider({
+    GitHub({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
+  callbacks: {
+    async signIn({ user, profile }: any) {
+      const { name, email, image } = user;
+      const { id, login, bio } = profile;
+
+      const existingUser = await client
+        .withConfig({ useCdn: false })
+        .fetch(AUTHOR_BY_GITHUB_ID_QUERY, { id });
+
+      if (!existingUser) {
+        await writeClient.create({
+          _type: "author",
+          id,
+          name,
+          username: login,
+          email,
+          image,
+          bio: bio || "",
+        });
+      }
+
+      return true;
+    },
+    async jwt({ token, profile }: any) {
+      if (profile?.id) {
+        const user = await client
+          .withConfig({ useCdn: false })
+          .fetch(AUTHOR_BY_GITHUB_ID_QUERY, { id: profile.id });
+
+        token.id = user?._id;
+      }
+
+      return token;
+    },
+    async session({ session, token }: any) {
+      return {
+        ...session,
+        id: token.id,
+        user: {
+          ...session.user,
+        },
+      };
+    },
+  },
 };
 
 export function auth() {
